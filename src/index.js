005518a3ae7603f4d18dce580ef0cbf1c4e362ef
jemel <jemel.defreitas@gmail.com>
2023-03-05 14:46:57 -0600

    commit tree e7ed6aa5be9c0318e8eaa2dbcc9c7eee222b3a97

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+document.addEventListener( "DOMContentLoaded" , () => {
+   let form = document.querySelector('form')
+   form.addEventListener('submit', (e) => {
+      e.preventDefault()
+      let newTask = document.getElementById('new-task-description')
+      handleNewTask(newTask.value)
+      form.reset()
+
+    })
+  })
+   function handleNewTask(newTask) {
+      let p = document.createElement("p")
+     let btn = document.createElement("button")
+    btn.textContent = "x"
+     p.textContent = `${newTask}`
+     p.appendChild(btn) 
+    document.querySelector("#list").appendChild(p)
+      
+     }
+     
+    
+    
+    
